2337cfa5eaf53056317d0524e131849ec7c3665f
Calia <percalia@qq.com>
2017-11-03 14:50:06 +0800

    salt base目录

---
+++ saltstack/install_zabbix-agent-rhel7.sls
@@ -0,0 +1,15 @@
+zabbix-agent:
+  file.managed:
+    - name: /app/script/install/zabbix-agent.sh
+    - source: salt://script/install_zabbix-agent-rhel7.sh
+    - mode: 744
+    - require:
+      - file: /app/script/install
+  cmd.run:
+    - name: /app/script/install/zabbix-agent.sh 
+
+/app/script/install:
+  file.directory:
+    - makedirs: true
+    - user: root
+    - group: root
---
+++ saltstack/script/install_zabbix-agent-rhel7.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# 在Red Hat7系统上安装zabbix-agent-3.4.3
+
+HOSTNAME = hostname
+
+#关闭selinux
+sed -i "s/SELINUX=enforcing/#SELINUX=enforcing\nSELINUX=disabled/g" /etc/selinux/config
+setenforce 0
+
+#安装zabbix-agent
+rpm -ivh http://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-agent-3.4.3-1.el7.x86_64.rpm
+
+#配置zabbix-agent
+sed -i "s/Server=127.0.0.1/#Server=127.0.0.1\nServer=192.168.3.10/g" /etc/zabbix/zabbix_agentd.conf
+sed -i "s/ServerActive=127.0.0.1/#ServerActive=127.0.0.1\nServerActive=192.168.3.10/g" /etc/zabbix/zabbix_agentd.conf
+sed -i "s/Hostname=Zabbix server/#Hostname=Zabbix server\nHostname=$HOSTNAME/g" /etc/zabbix/zabbix_agentd.conf
+
+#启动并设置开机启动
+systemctl enable zabbix-agent.service
+systemctl start zabbix-agent.service
